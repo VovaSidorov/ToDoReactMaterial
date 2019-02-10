10b9cdd54bc76d0bc151ae7b2e59b37cb88f542f
VovaSidorov <VovaCivic@gmail.com>
2019-02-10 17:12:01 +0200

    done status handler

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {bindActionCreators} from "redux";
+import {doneTask} from "../../store/actions";
+import {connect} from "react-redux";
 
 export const TaskList = (props) => {
 
-    const {tasks} = props;
+    const {tasks, setTaskDone} = props;
 
     return (
         <ul>
@@ -10,7 +13,7 @@ export const TaskList = (props) => {
                 <li key={index}>
                     {el.name}
                     {
-                        el.done?"":<button onClick={onDoneHandler}>Done</button>
+                        el.done?"":<button onClick={()=> setTaskDone(index)}>Done</button>
                     }
                     <button onClick={onRemoveHandler}>Remove</button>
                 </li>
@@ -28,4 +31,9 @@ const onRemoveHandler = () => {
 };
 
 const mapStateToProps = state => ({...state});
-const mapActionToProps = dispatch =>
+const mapActionToProps = dispatch =>{
+    return {
+        setTaskDone:task=>dispatch(doneTask(task))
+    }
+};
+export default connect(mapStateToProps,mapActionToProps)(TaskList);
