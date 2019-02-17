0d2242517f5e7885c0c816629aa79057d5c4accc
VovaSidorov <VovaCivic@gmail.com>
2019-02-17 16:09:37 +0200

    Create add task

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Header from "./Layouts";
 import TaskList from "./components/TaskList";
 import AddTask from "./components/AddTask";
+import TaskFilter from "./components/TaskFilter";
 
 export default props =>(
             <div>
                 <Header />
                 <AddTask/>
+                <TaskFilter/>
                 <TaskList tasks={props.tasks}/>
             </div>
 );
